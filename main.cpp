65b916d7622f2ad0217c830e0cc176bc74db1daa
YonatanEliyahu <yonataneliyahu16@gmail.com>
2023-07-12 11:05:19 +0300

    added timer funtionality to the game

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cctype>
 #include <vector>
+#include <chrono>
 #include "helper.h"
 #include "player.h"
 
@@ -12,15 +13,29 @@ int main()
     int mapSize;
     initMapSize(mapSize);
     Player p{};
+    int timeLimitSeconds = mapSize * 1.5; // Set the time limit in seconds
 
     // Create a 2D map using std::vector
     vector<vector<char>> map(mapSize, vector<char>(mapSize));
     initMap(map);
     bool gameOn = true;
-    cout << "move over the map using wasd \navoid monsters (@) and move arong walls (#) to get your prize ($)"<<endl<<endl<<endl;
+    cout << "move over the map using wasd \navoid monsters (@) and move arong walls (#) to get your prize ($)" << endl
+         << endl
+         << endl;
     displayMap(map);
+    // Start the timer
+    chrono::steady_clock::time_point startTime = chrono::steady_clock::now();
     while (gameOn)
     {
+        // Check the elapsed time
+        std::chrono::steady_clock::time_point currentTime = std::chrono::steady_clock::now();
+        std::chrono::duration<double> elapsedSeconds = currentTime - startTime;
+        if (elapsedSeconds.count() >= timeLimitSeconds)
+        {
+            std::cout << "Time's up!" << std::endl;
+            break; // Exit the game loop
+        }
+        std::cout << "time left " << (int)elapsedSeconds.count() << '/' << timeLimitSeconds << endl;
         gameOn = takeTurn(p, map);
         if (gameOn)
             displayMap(map);
